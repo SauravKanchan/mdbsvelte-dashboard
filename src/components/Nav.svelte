79462458dba6c19ab262713d86ac826c4803a5c8
SauravKanchan <sauravnk30@gmail.com>
2020-05-19 22:17:15 +0530

    first commit

---
+++ src/components/Nav.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let segment;
+  export let isOpen;
+  import {fly} from 'svelte/transition';
+  import MDBNavLink from 'mdbsvelte/src/MDBNavLink.svelte';
+
+
+</script>
+
+<style>
+  .sidebar {
+    min-width: 250px;
+    max-width: 250px;
+    min-height: 100vh;
+    z-index: 100;
+    position: fixed;
+  }
+</style>
+{#if isOpen}
+  <nav class="sidebar bg-dark  p-5" transition:fly="{{ x:-250, y: 0, duration: 500 }}">
+    <MDBNavLink class="text-white" active href="/">Home</MDBNavLink>
+    <MDBNavLink class="text-white" href="/about">About</MDBNavLink>
+    <MDBNavLink class="text-white" href="/blog">Blog</MDBNavLink>
+  </nav>
+{/if}
---
+++ src/routes/_layout.svelte
@@ -0,0 +1,33 @@
+<script>
+  import Nav from '../components/Nav.svelte';
+
+  import MDBRow from 'mdbsvelte/src/MDBRow.svelte';
+  import MDBNavbar from 'mdbsvelte/src/MDBNavbar.svelte'
+  import MDBNavbarBrand from 'mdbsvelte/src/MDBNavbarBrand.svelte'
+  import MDBBtn from 'mdbsvelte/src/MDBBtn.svelte'
+  import MDBNavbarNav from 'mdbsvelte/src/MDBNavbarNav.svelte'
+
+
+  let isOpen = true;
+  export let segment;
+</script>
+<style>
+  .active{
+    margin-left: 250px
+  }
+</style>
+
+<MDBRow class="wrapper">
+  <Nav {segment} {isOpen}/>
+  <div style="width: 100%" class:active={isOpen}>
+    <MDBNavbar color="primary-color" dark expand="md">
+      <MDBBtn color="light-blue" on:click={ ()=>isOpen = !isOpen}/>
+      <MDBNavbarNav right>
+        <MDBNavbarBrand>
+          <strong class="white-text">Admin</strong>
+        </MDBNavbarBrand>
+      </MDBNavbarNav>
+    </MDBNavbar>
+    <slot></slot>
+  </div>
+</MDBRow>
