31fc76d7f65744974cc58f1509173992f84a4e59
SauravKanchan <sauravnk30@gmail.com>
2020-05-22 20:10:42 +0530

    Add Modal Section

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,7 +2,9 @@
   import AdminCardSection1 from '../components/sections/AdminCardSection1.svelte'
   import BreadcrumbSection from '../components/sections/BreadcrumbSection.svelte'
   import ChartSection1 from '../components/sections/ChartSection1.svelte'
+  import ChartSection2 from '../components/sections/ChartSection2.svelte'
   import TableSection from '../components/sections/TableSection.svelte'
+  import ModalSection from '../components/sections/ModalSection.svelte'
   import MDBContainer from 'mdbsvelte/src/MDBContainer.svelte'
 </script>
 <MDBContainer fluid>
@@ -10,4 +12,6 @@
   <AdminCardSection1 />
   <ChartSection1 />
   <TableSection />
+  <ChartSection2 />
+  <ModalSection />
 </MDBContainer>
