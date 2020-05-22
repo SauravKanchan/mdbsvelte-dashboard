3e87308709fed112232c3e34414cdf5d9e404b7f
SauravKanchan <sauravnk30@gmail.com>
2020-05-22 19:15:21 +0530

    Add ChartSection1

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,11 @@
 <script>
   import AdminCardSection1 from '../components/sections/AdminCardSection1.svelte'
   import BreadcrumbSection from '../components/sections/BreadcrumbSection.svelte'
+  import ChartSection1 from '../components/sections/ChartSection1.svelte'
   import MDBContainer from 'mdbsvelte/src/MDBContainer.svelte'
 </script>
 <MDBContainer fluid>
   <BreadcrumbSection />
   <AdminCardSection1 />
+  <ChartSection1 />
 </MDBContainer>
